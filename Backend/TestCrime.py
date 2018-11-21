18238455e8d6a35c3319230c98bd17a254fd25af
admin <admin@USCS-Mac173.local>
2018-11-20 20:44:47 -0800

    Backend changes

--- Backend/TestCrime.py
+++ Backend/TestCrime.py
@@ -3,15 +3,34 @@ from flask import jsonify
 app = Flask(__name__)
 import pymongo
 from pymongo import MongoClient
+
+from bson import Binary, Code
+from bson.json_util import dumps
+import json
+
+connection = MongoClient("mongodb://dbuser:dbuser1@ds039707.mlab.com:39707/safety_predict")
+db = connection['safety_predict']
+collection = db.crime
+
+
 @app.route("/", methods=['GET'])
 def fetch_crime():
-	#client = MongoClient()
-	#connection = Connection()
-	connection = MongoClient("mongodb://dbuser:dbuser1@ds039707.mlab.com:39707/safety_predict")
-	#connection = MongoClient("mongodb://ds039707.mlab.com:39707/safety_predict")
-	db = connection['safety_predict']
-	db = connection['safety_predict']
-	collection = db.crime
-	#for post in collection.find():
-	#	print(post)
 	return jsonify(collection.find().count()),200
+
+@app.route("/getCrimeData", methods=['GET'])
+def fetchDayNightData():
+	result = collection.find({"City": "Los Angeles","Area Name" : "77th Street"})
+	jsonResult={}
+	jsonResult['day'] = 0
+	jsonResult['night'] = 0
+	for item in result:
+		timeOfIncident = int(item["Time Occurred"])
+		if timeOfIncident >= 500 and timeOfIncident <= 1800:
+			jsonResult['day']=jsonResult['day']+1
+		else:
+			jsonResult['night']=jsonResult['night']+1
+	#resultCount = [day, night]
+	return dumps(jsonResult)
+
+
+
